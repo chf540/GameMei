a63ccfca1683a7e55379b3a60577c98e3fd496ff
chf540 <334102254@qq.com>
2016-02-01 14:40:58 +0800

    更新SDK

---
+++ app/src/main/java/gamemei/qiyun/com/gamemei/activity/WBShareActivity.java
@@ -0,0 +1,9 @@
+package gamemei.qiyun.com.gamemei.activity;
+
+import com.umeng.socialize.media.WBShareCallBackActivity;
+
+/**
+ * Created by wangfei on 15/12/3.
+ */
+public class WBShareActivity extends WBShareCallBackActivity{
+}
