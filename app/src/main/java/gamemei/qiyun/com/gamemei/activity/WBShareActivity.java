0ce524c3be8497defaa2ba26d97910a106283f98
chf540 <334102254@qq.com>
2016-02-02 14:42:32 +0800

    添加友盟分享

--- app/src/main/java/gamemei/qiyun/com/gamemei/activity/WBShareActivity.java
+++
@@ -1,9 +0,0 @@
-package gamemei.qiyun.com.gamemei.activity;
-
-import com.umeng.socialize.media.WBShareCallBackActivity;
-
-/**
- * Created by wangfei on 15/12/3.
- */
-public class WBShareActivity extends WBShareCallBackActivity{
-}
